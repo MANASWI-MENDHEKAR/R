fa808ab48d901f854cb484ba5bb7346b60085a0d
MANASWI-MENDHEKAR <154888424+MANASWI-MENDHEKAR@users.noreply.github.com>
2025-01-30 22:16:38 +0530

    Update Problem_23.R

--- Assignment1/Problem_23.R
+++ Assignment1/Problem_23.R
@@ -1,1 +1,5 @@
+vec_23_extract <- vec4[c(1,3)]
+vec_23_remaining <- vec4[-c(1,3)]
+vec_23_reconstructed <- c(vec_23_extract[1], vec_23_remaining[1], vec_23_extract[2], vec_23_remaining[-1])
+print(vec_23_reconstructed)
 
