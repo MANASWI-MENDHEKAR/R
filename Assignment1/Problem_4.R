33737900c1360ce20226c51dff1a8093a88d0868
MANASWI-MENDHEKAR <154888424+MANASWI-MENDHEKAR@users.noreply.github.com>
2025-01-30 13:00:29 +0530

    Update Problem_4.R

--- Assignment1/Problem_4.R
+++ Assignment1/Problem_4.R
@@ -1,16 +1,2 @@
-#1
-x<-seq(from=6, to=12 , by=1)
-print(x)
-
-#2
-r<-rep(5.3,times=3)
-print(r)
-
-#3
-x<--3
-print(x)
-
-#4
-s<-seq(from=102,to=l,length.out=9)
-print(s)
-
+vec4 <- c(6:12,rep(5.3, 3),-3,seq(102, length(vec3), length.out = 9))
+print(vec4)
