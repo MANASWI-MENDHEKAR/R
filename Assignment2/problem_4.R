bb48de79f972be8a3effa0cfcf4e87273e7dc874
MANASWI-MENDHEKAR <154888424+MANASWI-MENDHEKAR@users.noreply.github.com>
2025-02-04 00:51:55 +0530

    Update problem_4.R

--- Assignment2/problem_4.R
+++ Assignment2/problem_4.R
@@ -1,1 +1,10 @@
+# What does R return if you delete the fourth row and the first column from (3.)? Use matrix to ensure the result is a single-column matrix, rather than a vector.
 
+b<-matrix(a[-4,-1],ncol=1)
+print(b)
+
+#output :
+    # [,1]
+#[1,]  0.9
+#[2,]  3.1
+#[3,]  6.5
