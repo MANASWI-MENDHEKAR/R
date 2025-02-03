1aad175fe93578afe803bef519ccce75397bee42
MANASWI-MENDHEKAR <154888424+MANASWI-MENDHEKAR@users.noreply.github.com>
2025-02-04 00:47:29 +0530

    Update problem_3.R

--- Assignment2/problem_3.R
+++ Assignment2/problem_3.R
@@ -1,1 +1,12 @@
+# Overwrite the second column of the matrix from (1.) with that same column sorted from smallest to largest.
+
+a[,2]<-sort(a[,2])
+print(a)
+
+#output :  [,1] [,2]
+#[1,]  4.3  0.9
+#[2,]  8.2  3.1
+#[3,]  3.2  6.5
+#[4,]  1.6  8.2
+
 
