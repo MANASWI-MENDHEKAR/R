16f89bf78f7ecd03dfa3d353ac67a769606736fd
MANASWI-MENDHEKAR <154888424+MANASWI-MENDHEKAR@users.noreply.github.com>
2025-02-05 17:38:53 +0530

    Update problem_17.R

--- Assignment2/problem_17.R
+++ Assignment2/problem_17.R
@@ -1,1 +1,13 @@
+# 17. Sort the first column of the matrix from question (15.) in descending order. Keep other columns
+# unchanged.
 
+mat5x3 <- mat5x3[order(-mat5x3[, 1]), ]
+print(mat5x3)
+
+#output :  [,1] [,2] [,3]
+# [1,]   16   17   18
+# [2,]    5   10   15
+# [3,]    4    9   14
+# [4,]    3    8   13
+# [5,]    2    7   12
+# [6,]    1    6   11
