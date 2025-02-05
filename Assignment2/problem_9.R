fbbcc94b9ff7f32054928c3e99a9b4efeafd1c3d
MANASWI-MENDHEKAR <154888424+MANASWI-MENDHEKAR@users.noreply.github.com>
2025-02-05 17:17:35 +0530

    Update problem_9.R

--- Assignment2/problem_9.R
+++ Assignment2/problem_9.R
@@ -1,1 +1,12 @@
+# 
 
+A_mat <- diag(c(2, 3, 5, -1))
+A_inv <- solve(A_mat)
+result9 <- A_inv %*% A_mat - diag(4)
+print(result9)
+
+# output :  [,1] [,2] [,3] [,4]
+#     [1,]    0    0    0    0
+#     [2,]    0    0    0    0
+#     [3,]    0    0    0    0
+#     [4,]    0    0    0    0
