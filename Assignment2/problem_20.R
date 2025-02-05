6a3bb8667f94fe491ee933089b2b161415dcad6b
MANASWI-MENDHEKAR <154888424+MANASWI-MENDHEKAR@users.noreply.github.com>
2025-02-05 17:43:59 +0530

    Update problem_20.R

--- Assignment2/problem_20.R
+++ Assignment2/problem_20.R
@@ -1,1 +1,14 @@
+# 20. Replace the elements at positions (2,1), (2,3), (5,1), and (5,3) in the matrix from question (17.)
+# with the average of the four corner elements of that matrix.
 
+corner_avg <- mean(c(mat5x3[1, 1], mat5x3[1, 3], mat5x3[5, 1], mat5x3[5, 3]))
+mat5x3[c(2, 5), c(1, 3)] <- corner_avg
+print(mat5x3)
+
+#output:   [,1] [,2] [,3]
+# [1,]   16   17   18
+# [2,]   12   10   12
+# [3,]    4    9   14
+# [4,]    3    8   13
+# [5,]   12    7   12
+# [6,]    1    6   11
