1321ccb883610e394d68fb5c2704b9275145c3c1
MANASWI-MENDHEKAR <154888424+MANASWI-MENDHEKAR@users.noreply.github.com>
2025-02-05 17:47:42 +0530

    Update problem_21.R

--- Assignment2/problem_21.R
+++ Assignment2/problem_21.R
@@ -1,1 +1,10 @@
+# 21
 
+mat_x <- matrix(c(2, 3, 5, 7), nrow = 2, byrow = TRUE)
+mat_y <- matrix(c(5, 15, 10, 20), nrow = 2, byrow = TRUE)
+result_21 <- 3 * mat_x - mat_y
+print(result_21)
+
+#output :      [,1] [,2]
+# [1,]    1   -6
+# [2,]    5    1
