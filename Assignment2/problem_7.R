473807f66fc213ec66c03185d3dc877decbaed14
MANASWI-MENDHEKAR <154888424+MANASWI-MENDHEKAR@users.noreply.github.com>
2025-02-05 17:09:21 +0530

    Update problem_7.R

--- Assignment2/problem_7.R
+++ Assignment2/problem_7.R
@@ -1,1 +1,14 @@
+# 7. calculate the following
+
+mat_a <- matrix(c(1, 2, 7, 2, 4, 6), nrow = 3, byrow = TRUE)
+mat_b <- matrix(c(10, 20, 30, 40, 50, 60), nrow = 3, byrow = TRUE)
+result <- (2/7) * (mat_a - mat_b)
+print(result)
+
+#output :          [,1]       [,2]
+#          [1,]  -2.571429  -5.142857
+#          [2,]  -6.571429 -10.857143
+#          [3,] -13.142857 -15.428571
+
+
 
