b4e03387718d67fb4bb46e213b6011032f8a0c06
MANASWI-MENDHEKAR <154888424+MANASWI-MENDHEKAR@users.noreply.github.com>
2025-02-05 17:57:23 +0530

    Update problem_24.R

--- Assignment2/problem_24.R
+++ Assignment2/problem_24.R
@@ -1,1 +1,12 @@
+# 24. Construct a three-dimensional array with four layers, each a 3 x 3 matrix filled with random
+# numbers between 1 and 9. Then, extract the elements of the first row of the third column across all
+# layers and store them as a new vector.
 
+
+set.seed(42) 
+arr_3d <- array(sample(1:9, 3*3*4, replace = TRUE), dim = c(3, 3, 4))
+
+extracted_vector <- arr_3d[1, 3, ]
+print(extracted_vector)
+
+#output : [1] 1 9 1 4
