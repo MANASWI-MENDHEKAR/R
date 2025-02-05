47651972026257d5637fd83567aee2f9e51e619c
MANASWI-MENDHEKAR <154888424+MANASWI-MENDHEKAR@users.noreply.github.com>
2025-02-05 17:55:18 +0530

    Update problem_23.R

--- Assignment2/problem_23.R
+++ Assignment2/problem_23.R
@@ -1,1 +1,11 @@
+# 23.
 
+B_mat <- diag(c(1, 2, -2))
+B_inv <- solve(B_mat)
+result_23 <- B_inv %*% B_mat - diag(3)
+print(result_23)
+
+#output:  [,1] [,2] [,3]
+# [1,]    0    0    0
+# [2,]    0    0    0
+# [3,]    0    0    0
