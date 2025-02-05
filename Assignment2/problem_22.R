01d035bcbe28f358044be416cd5bce678ef37284
MANASWI-MENDHEKAR <154888424+MANASWI-MENDHEKAR@users.noreply.github.com>
2025-02-05 17:53:27 +0530

    Update problem_22.R

--- Assignment2/problem_22.R
+++ Assignment2/problem_22.R
@@ -1,1 +1,24 @@
+# 22. 
+
+A <- matrix(c(1, 2, 3, 4), nrow = 2, byrow = TRUE)
+B <- matrix(c(5, 6), nrow = 2, byrow = TRUE)
+
+if (ncol(A) == nrow(B)) print(A %*% B)
+
+#output : [,1]
+# [1,]   17
+# [2,]   39
+
+if (ncol(t(A)) == nrow(B)) print(t(A) %*% B)
+
+#output : [,1]
+# [1,]   23
+# [2,]   34
+
+if (ncol(t(B)) == nrow(A %*% t(A))) print(t(B) %*% (A %*% t(A)))
+
+#output:      [,1] [,2]
+# [1,]   91  205
+
+
 
