bbf7b92d9d9755f84f33b3392eea0beeaf35f1da
MANASWI-MENDHEKAR <154888424+MANASWI-MENDHEKAR@users.noreply.github.com>
2025-02-05 17:05:23 +0530

    Update problem_6.R

--- Assignment2/problem_6.R
+++ Assignment2/problem_6.R
@@ -1,1 +1,18 @@
+# 6. Overwrite, in this order, the elements of (3.) at positions (4;2), (1;2), (4;1), and (1;1) with -1/2
+# of the two values on the diagonal of (e).
 
+
+ diag_values <- diag(mat3)
+ replacement_values <- -0.5 * diag_values
+ mat1[4, 2] <- replacement_values[2]
+ mat1[1, 2] <- replacement_values[1]
+ mat1[4, 1] <- replacement_values[2]
+ mat1[1, 1] <- replacement_values[1]
+ print(mat1)
+
+
+# output:    [,1] [,2]
+# [1,] -1.6 -1.6
+# [2,]  8.2  3.1
+# [3,]  3.2  6.5
+# [4,] -4.1 -4.1
