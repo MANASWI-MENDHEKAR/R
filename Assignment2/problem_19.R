1eb34ea9b55af5bff3c85c5e178277f1dcbcc2c2
MANASWI-MENDHEKAR <154888424+MANASWI-MENDHEKAR@users.noreply.github.com>
2025-02-05 17:41:45 +0530

    Update problem_19.R

--- Assignment2/problem_19.R
+++ Assignment2/problem_19.R
@@ -1,1 +1,9 @@
+# 19. From the matrix in question (17.), create a new 2 x 2 matrix using the top four elements of the
+# last column.
 
+mat19 <- matrix(mat5x3[1:2, 3], nrow = 2)
+print(mat19)
+
+#output :  [,1]
+# [1,]   18
+# [2,]   15
