536a4fffac1a2c7e97fc40f3e15b0b124a46d1a5
MANASWI-MENDHEKAR <154888424+MANASWI-MENDHEKAR@users.noreply.github.com>
2025-04-28 17:10:53 +0530

    Create problem11.R

---
+++ Assignment4/problem11.R
@@ -0,0 +1,13 @@
+nested_list<-list(numbers=c(1,2,3),details=list(characters=c("a","b","c")),nums=c(4,5,6))
+nested_list
+
+#output:   $numbers
+          #[1] 1 2 3
+
+          #$details
+          #$details$characters
+          #[1] "a" "b" "c"
+
+
+          #$nums
+          #[1] 4 5 6
