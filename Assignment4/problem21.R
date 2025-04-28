0ed7062c753a740b9cae4727c7d621e07f99208e
MANASWI-MENDHEKAR <154888424+MANASWI-MENDHEKAR@users.noreply.github.com>
2025-04-28 22:15:43 +0530

    Create problem21.R

---
+++ Assignment4/problem21.R
@@ -0,0 +1,8 @@
+merge_lists <- function(list1, list2) {
+  return(list(list1 = list1, list2 = list2))
+}
+merge_lists
+
+#output:  function(list1, list2) {
+  #return(list(list1 = list1, list2 = list2))
+#}
